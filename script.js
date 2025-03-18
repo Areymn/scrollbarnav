22ca71c3eec7a5780b9d4c527234312d28575202
Areymn <alexielreyesmartin@gmail.com>
2025-03-18 18:13:41 +0100

    JavaScript integration

--- script.js
+++ script.js
@@ -1,4 +1,1 @@
-window.addEventListener('scroll', () => {
-	document.getElementById('navlist').style.top =
-		document.documentElement.scrollTop > 20 ? '0' : '-60px';
-});
+window.addEventListener('scroll', () => {document.getElementById('navlist').style.top = document.documentElement.scrollTop > 20 ? '0' : '-60px';});
